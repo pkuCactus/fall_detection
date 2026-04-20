e962dfb0b4e955a629f9c11fc9eea50d2231f2a0
pkuCactus <jianzhonghe@outlook.com>
2026-04-20 12:57:52 +0800

    refactor(yolo): remove redundant BatchNorm2d parameter overrides

--- src/fall_detection/models/yolo.py
+++ src/fall_detection/models/yolo.py
@@ -295,8 +295,7 @@ class CustomDetectionModel(DetectionModel):
             if t is nn.Conv2d:
                 pass  # nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
             elif t is nn.BatchNorm2d:
-                m.eps = 1e-3
-                m.momentum = 0.03
+                pass
             elif t in [nn.Hardswish, nn.LeakyReLU, nn.ReLU, nn.ReLU6, nn.SiLU]:
                 m.inplace = True
 
